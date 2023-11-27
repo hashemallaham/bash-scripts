726f7e06a1ae673eced6525e5d5f5ae05dab83be
Hashem Allaham <146624907+hashemallaham@users.noreply.github.com>
2023-11-27 18:24:04 +0300

    Create ssl-generator.sh
    first commit

---
+++ ssl-generator.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#############################################################################
+# Script name: ssl-generator.sh                                             #
+# Version: 1.0.1                                                            #
+# Description: generate a self-signed certificate                           #
+# Author: Eng. Hashem Allaham (Syria)                                       #
+# Email: hashem.allaham@gmail.com                                           #
+#############################################################################
+
+
+# Set the common name for the certificate
+read -p "Enter the domain name: " COMMON_NAME
+
+# Generate a private key
+openssl genrsa -out server.key 2048
+
+# Generate a certificate signing request (CSR)
+openssl req -new -key server.key -out COMMON_NAME.csr -subj "/CN=$COMMON_NAME"
+
+# Generate a self-signed certificate
+openssl x509 -req -days 365 -in server.csr -signkey COMMON_NAME.key -out COMMON_NAME.crt
