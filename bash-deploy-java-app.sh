49caa5365ed04b92ebf27f1d45ac97917af068e4
Hashem Allaham <146624907+hashemallaham@users.noreply.github.com>
2024-01-08 00:27:34 +0300

    Create bash-deploy-java-app.sh

---
+++ bash-deploy-java-app.sh
@@ -0,0 +1,23 @@
+﻿#!/bin/bash
+# configuration
+REPO_URL="https://github.com/hashemallaham/myproject1.git"
+REPO_NAME="myproject1"
+WORKDIR="/app"
+BACKUP_DIR="/backup/app"
+
+# Backup existing deployment (useful in case of rolling back)
+echo "Backing up current deployment..."
+sudo mkdir -p $BACKUP_DIR
+tar -czf $BACKUP_DIR/backup_$(date +%F-%H-%M-%S).tar.gz $WORKDIR && rm -rf $WORKDIR
+
+# Pull the latest code of the project
+mkdir -p $WORKDIR
+cd $WORKDIR
+git clone $REPO_URL
+cd $REPO_NAME
+
+# Build or compile your application if needed echo "Building or Packaging the application..." ./mvnw package
+# Running the Application
+java -jar target/*.jar
+
+# The appliction can be accessed via http://localhost:8080
